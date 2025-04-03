8e0b7ee24f32ccc6d1ecdab975f0c5c6d3a98091
Олег Пак <oleg.pak1996@gmail.com>
2025-04-03 23:20:27 +0300

    +

--- lib/presentations/screens/training_screen.dart
+++ lib/presentations/screens/training_screen.dart
@@ -5,6 +5,10 @@ class TrainingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text('Тренировка'),
+      ),
+    );
   }
 }
