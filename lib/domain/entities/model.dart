15689f1a6c47ec8dde38d38f4ea0fd09d3a1732e
Олег Пак <oleg.pak1996@gmail.com>
2025-05-13 22:59:13 +0300

    +

---
+++ lib/domain/entities/model.dart
@@ -0,0 +1,13 @@
+class Exercise {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final List<String> muscleGroups;
+  
+  Exercise({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.muscleGroups,
+  });
+}
