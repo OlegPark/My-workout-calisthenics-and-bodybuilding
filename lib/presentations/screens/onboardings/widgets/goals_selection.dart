d10e498c8a535ac089283488774382aa01d7c957
Олег Пак <oleg.pak1996@gmail.com>
2025-07-01 16:33:11 +0300

    +

--- lib/presentations/screens/onboardings/widgets/goals_selection.dart
+++ lib/presentations/screens/onboardings/widgets/goals_selection.dart
@@ -83,32 +83,36 @@ class GoalsSelection extends StatelessWidget {
         SizedBox(height: 8),
         GestureDetector(
           onTap: () => _showGoalBottomSheet(context, key, options, primaryColor),
-          child: Container(
-            height: 46,
-            margin: EdgeInsets.symmetric(horizontal: 15),
-            decoration: BoxDecoration(
-              color: primaryColor,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(25),
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    selectedText,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+              child: ColoredBox(
+                color: primaryColor,
+                child: SizedBox(
+                  height: 46,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          selectedText,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                      ],
                     ),
                   ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.black,
-                    size: 16,
-                  ),
-                ],
+                ),
               ),
             ),
           ),
@@ -127,88 +131,95 @@ class GoalsSelection extends StatelessWidget {
       context: context,
       backgroundColor: Colors.transparent,
       builder: (BuildContext context) {
-        return Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(25),
-              topRight: Radius.circular(25),
-            ),
+        return ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 12),
-                width: 40,
-                height: 4,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(2),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Text(
-                      'Выберите диапазон',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+          child: ColoredBox(
+            color: Colors.white,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 12),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(2),
+                    child: ColoredBox(
+                      color: Colors.grey[300]!,
+                      child: SizedBox(
+                        width: 40,
+                        height: 4,
                       ),
                     ),
-                    SizedBox(height: 20),
-                    ...options.asMap().entries.map((entry) {
-                      final index = entry.key;
-                      final option = entry.value;
-                      final isSelected = goals[key] == index;
-                      
-                      return GestureDetector(
-                        onTap: () {
-                          final newGoals = Map<String, int>.from(goals);
-                          newGoals[key] = index;
-                          onGoalsChanged(newGoals);
-                          Navigator.pop(context);
-                        },
-                        child: Container(
-                          width: double.infinity,
-                          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                          margin: EdgeInsets.only(bottom: 10),
-                          decoration: BoxDecoration(
-                            color: isSelected ? primaryColor : Colors.grey[100],
-                            borderRadius: BorderRadius.circular(15),
-                            border: isSelected 
-                              ? Border.all(color: primaryColor, width: 2)
-                              : null,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                option,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: isSelected ? Colors.black : Colors.grey[700],
-                                  fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Выберите диапазон',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ...options.asMap().entries.map((entry) {
+                        final index = entry.key;
+                        final option = entry.value;
+                        final isSelected = goals[key] == index;
+                        
+                        return GestureDetector(
+                          onTap: () {
+                            final newGoals = Map<String, int>.from(goals);
+                            newGoals[key] = index;
+                            onGoalsChanged(newGoals);
+                            Navigator.pop(context);
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 10),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: ColoredBox(
+                                color: isSelected ? primaryColor : Colors.grey[100]!,
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          option,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: isSelected ? Colors.black : Colors.grey[700],
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        if (isSelected)
+                                          Icon(
+                                            Icons.check,
+                                            color: Colors.black,
+                                            size: 20,
+                                          ),
+                                      ],
+                                    ),
+                                  ),
                                 ),
                               ),
-                              if (isSelected)
-                                Icon(
-                                  Icons.check,
-                                  color: Colors.black,
-                                  size: 20,
-                                ),
-                            ],
+                            ),
                           ),
-                        ),
-                      );
-                    }).toList(),
-                  ],
+                        );
+                      }).toList(),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
