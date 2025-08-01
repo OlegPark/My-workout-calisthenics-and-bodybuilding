8f8b3ab077af142a229e4b09f379be1c89f9ad11
Олег Пак <oleg.pak1996@gmail.com>
2025-08-01 16:57:44 +0300

    +

--- lib/core/navigation/router.dart
+++ lib/core/navigation/router.dart
@@ -1,5 +1,7 @@
 import 'package:go_router/go_router.dart';
-import 'package:my_workout_cab/features/onboarding/presentation/widgets/onboarding1.dart';
+import 'package:my_workout_cab/core/navigation/gnav_bar.dart';
+import 'package:my_workout_cab/features/create/presentation/widgets/exercises_screen.dart';
+
 
 import 'package:my_workout_cab/features/onboarding/presentation/widgets/onboarding2.dart';
 
@@ -9,7 +11,7 @@ final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => Onboarding1(),
+      builder: (context, state) => ExercisePage(),
     ),
     GoRoute(
       path: '/onboarding2',
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import app_links
 import cloud_firestore
 import firebase_core
-import firebase_remote_config
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -19,7 +18,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
